0780612ae87c837d7348befb9c7c03152ee4157a
mh-sudo <79376154+mh-sudo@users.noreply.github.com>
2026-07-14 19:24:29 +0600

    Update NetBar formula to v1.0.1

--- Casks/netbar.rb
+++ Casks/netbar.rb
@@ -1,8 +1,8 @@
 cask "netbar" do
-  version "1.0.0"
-  sha256 "85ddf756942d5709b09a638f345cf017042e4f18568bea41fb935be3a15c7d98"
+  version "1.0.1"
+  sha256 "dac4193614728bb1476d1a5a019ab5b813e938076c074769af57c9d640964073"
 
-  url "https://github.com/mh-sudo/NetBar/releases/download/v1.0.0/NetBar-1.0.0.zip"
+  url "https://github.com/mh-sudo/NetBar/releases/download/v1.0.1/NetBar-1.0.1.zip"
   name "NetBar"
   desc "Minimalist menubar app showing network speed and IP country flag"
   homepage "https://github.com/mh-sudo/NetBar"
